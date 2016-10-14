42d33355b5eae0d348c2d3b31c6496357fac7b52
Samigos <s.kaniras@gmail.com>
2016-10-14 22:27:34 +0300

    project init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ALProject
+//
+//  Created by Sotiris Kaniras on 14/10/16.
+//  Copyright © 2016 com.me. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    
+    return 0;
+}
