8a4671c60a1f5752da4b6a8f90a6980021762ae3
Samigos <s.kaniras@gmail.com>
2016-10-15 22:33:54 +0300

    check for all other arguments

--- main.cpp
+++ main.cpp
@@ -12,27 +12,29 @@
 
 using namespace std;
 
-void createRandomBitString();
-
 int main(int argc, const char * argv[]) {
     int k = 4, L = 5;
-    string d;
+    string d, o, q;
     
     int index;
     
-    for (index = 0; index < argc; index++) {
-        if (strcmp(argv[index], "-k") == 0) {
+    for (index = 1; index < argc; index++) {
+        if (strcmp(argv[index], "-d") == 0) {
+            d = string(argv[index+1]);
+        }
+        else if (strcmp(argv[index], "-q") == 0) {
+            q = string(argv[index+1]);
+        }
+        else if (strcmp(argv[index], "-k") == 0) {
             k = atoi(argv[index+1]);
         }
         else if (strcmp(argv[index], "-L") == 0) {
             L = atoi(argv[index+1]);
         }
-        else if (strcmp(argv[index], "-d") == 0) {
-            d = string(argv[index+1]);
+        else if (strcmp(argv[index], "-o") == 0) {
+            o = string(argv[index+1]);
         }
     }
     
-    cout << k << ", " << L << endl;
-
     return 0;
 }
