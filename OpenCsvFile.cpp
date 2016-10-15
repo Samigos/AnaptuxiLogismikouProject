7eaad7172c06d8a45f79824a5b8e48c3543020b1
Zeaniel <at_stavros@yahoo.gr>
2016-10-15 23:25:03 +0300

    hamming dataset allages

--- OpenCsvFile.cpp
+++ OpenCsvFile.cpp
@@ -2,14 +2,14 @@
     #include <sstream>
     #include <string>
     #include <fstream>
-
-        ifstream file("path"); // tha valoume edw to path pou theloume
-        file.ignore ( std::numeric_limits<std::streamsize>::max(), '\n' );
-        string line = "";
-        while (getline(file, line))
-	  {
-            stringstream strstr(line);
-            int pos = line.find_first_of("")
-            line= line.substr(pos+1)
-          }
-    
+       
+	std::ifstream file("path"); 
+	file.ignore ( std::numeric_limits<std::streamsize>::max(), '\n' );
+        std::string line = "";
+	while (getline(file, line))
+		{
+		stringstream strstr(line);
+		int pos = line.find_first_of("");
+		line= line.substr(pos+1);
+		cout << line << endl;
+		}
