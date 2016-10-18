318c791ff6dc070510cc1ac7d64109f91dcadcfc
Zeaniel <at_stavros@yahoo.gr>
2016-10-19 00:24:14 +0300

    kapoies allages

--- OpenCsvFile.cpp
+++ OpenCsvFile.cpp
@@ -2,14 +2,15 @@
     #include <sstream>
     #include <string>
     #include <fstream>
+      
+
        
-	std::ifstream file("path"); 
+	std::ifstream file("path"); // eisodos tou arxeiou
 	file.ignore ( std::numeric_limits<std::streamsize>::max(), '\n' );
         std::string line = "";
 	while (getline(file, line))
 		{
-		stringstream strstr(line);
-		int pos = line.find_first_of("");
+		int pos = line.find_first_of("\tab");
 		line= line.substr(pos+1);
 		cout << line << endl;
 		}
